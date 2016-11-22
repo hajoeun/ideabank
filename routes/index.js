66405a4788aad13a66ecdb392b08cd1e8f5c43de
Joeun <imjoeunha@gmail.com>
2016-11-22 18:58:08 +0900

    table connection and created input form

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,17 @@ var connectionString = 'joeun://localhost:5432/joeundb';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.send('<h1>hello world!</h1>');
   res.send(_.T('','\
-    h1 hello world\
+    div\
+      h2 idea bank prototype\
+      form[action=/api/v1/iposts method=post]\
+        label.control-label 제목\
+        input.form-control[type=text name=title placeholder="제목을 입력하세요."]\
+        label.control-label 유저 ID\
+        input.form-control[type=text name=u_id placeholder="유저 id"]\
+        label.control-label 내용\
+        textarea.form-control[type=number name=contents placeholder="내용"]\
+        button[type=submit] 완료\
   ')());
 });
 
@@ -19,7 +27,7 @@ function showResult(query, res, done, results) {
 
   query.on('end', function() {
     done();
-    return res.json(results);
+    return res.redirect(303, '/');
   });
 }
 
@@ -38,6 +46,19 @@ router.post('/api/v1/users', function(req, res, next) {
     if (err) { return error(err, res, done); }
 
     client.query('insert into users(name, age) values($1, $2)', [data.name, data.age]);
+
+    return showResult(client.query('select * from users order by id asc'), res, done, results);
+  })
+});
+
+router.post('/api/v1/iposts', function(req, res, next) {
+  var results = [];
+  var data = { title: req.body.title, contents: req.body.contents, u_id: req.body.u_id };
+
+  pg.connect(connectionString, function(err, client, done) {
+    if (err) { return error(err, res, done); }
+    // client.query('insert into icontents(p_id') // 여기에 데이터를 집어 넣으면됨! 콘텐츠 데이터!
+    client.query('insert into iposts(title, content_id, user_id, create_date) values($1, $2, $3, current_date)', [data.title, data.u_id]);
 
     return showResult(client.query('select * from users order by id asc'), res, done, results);
   })
